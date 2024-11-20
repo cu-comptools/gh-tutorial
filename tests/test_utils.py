cb7612c91238ce9d3544cff6439ea4bb0b5dad08
Fruzsina Agocs <agocs.fruzsina@gmail.com>
2024-11-20 14:17:34 -0700

    Fix syntax

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,5 +7,5 @@ def test_quadroots_1():
     roots = quadroots(a, b, c)
     x1_exact = 2.0
     x2_exact = 1.0
-    assert np.abs(roots[0] - x1_exact) < 1e-14 && np.abs(roots[0] - x2_exact) < 1e-14 
+    assert (np.abs(roots[0] - x1_exact) < 1e-14) and (np.abs(roots[1] - x2_exact)) < 1e-14 
 
